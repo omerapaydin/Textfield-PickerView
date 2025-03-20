a0f1bb009ff6a396f38765135d421e25abe82b75
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-20 20:42:55 +0300

    First

--- Textfield&PickerView/ViewController.swift
+++ Textfield&PickerView/ViewController.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
 
+    var pickerView:UIPickerView!
+    var ulkeler: [String] = ["Türkiye", "Asya", "Afrika", "Antarktika"]
+    
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        textField.inputView = pickerView
+        
+        
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ulkeler.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ulkeler[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textField.text = ulkeler[row]
+    }
+    
+    
 
 
 }
